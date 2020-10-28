9fc79c1da920749567d41f79106befcb17544f3e
Albert Suarez <alsumo95@gmail.com>
2020-10-28 19:51:18 +0100

    :hammer: Added sh script for downloading models from drive

---
+++ inference/gdrive_download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+rm -rf /tmp/cookies.txt
