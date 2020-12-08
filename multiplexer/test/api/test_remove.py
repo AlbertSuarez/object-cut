f5410fe1d940b5d1e8b11adad1aa05f4adeda52b
Albert Suarez <alsumo95@gmail.com>
2020-12-08 11:21:18 +0100

    :white_check_mark: Changed image URL test for the raven one

--- multiplexer/test/api/test_remove.py
+++ multiplexer/test/api/test_remove.py
@@ -8,7 +8,7 @@ class MultiplexerRemoveTest(BaseTestClass):
 
     def setUp(self):
         self.secret_access = env.get_secret_access()
-        self.img_url = 'https://objectcut.com/docs/images/object-cut.png'
+        self.img_url = 'https://objectcut.com/assets/img/raven.jpg'
         self.img_url_wrong = 'https://example.com/not-existing.jpg'
         self.img_base64_wrong = 'not-a-base64'
         self.img_path = os.path.join('test', 'data', 'person.jpg')
